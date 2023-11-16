67432da9a8afafbc44de98da6a4705192df21a47
Benefrancis <benefrancis@gmail.com>
2023-11-16 13:18:28 -0300

    Padronizando o ExceptionHandling

--- src/main/java/br/com/fiap/infra/configuration/exception/ConstraintViolationExceptionMapper.java
+++ src/main/java/br/com/fiap/infra/configuration/exception/ConstraintViolationExceptionMapper.java
@@ -5,6 +5,7 @@ import jakarta.json.JsonObject;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 import jakarta.ws.rs.ext.ExceptionMapper;
@@ -47,6 +48,9 @@ public class ConstraintViolationExceptionMapper implements ExceptionMapper<Const
 
         JsonObject errorJsonEntity = jsonObject.add( "ERRORS", jsonArray.build() ).build();
 
-        return Response.status( Response.Status.BAD_REQUEST ).entity( errorJsonEntity ).build();
+        return Response.status( Response.Status.BAD_REQUEST )
+                .entity( errorJsonEntity )
+                .type( MediaType.APPLICATION_JSON_TYPE )
+                .build();
     }
 }
